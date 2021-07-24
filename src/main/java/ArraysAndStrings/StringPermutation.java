7e97d7adb89fb7fb313db8fd848703cd734f2a0c
19092 <19092@esp.mr>
2021-07-24 15:27:23 +0000

    some changes

--- src/main/java/ArraysAndStrings/StringPermutation.java
+++ src/main/java/ArraysAndStrings/StringPermutation.java
@@ -1,7 +1,6 @@
 package ArraysAndStrings;
 
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class StringPermutation {
 //    public static String sortString(String inputString)
@@ -22,13 +21,29 @@ public class StringPermutation {
 //    }
 
       public static boolean checkPermutation2(String source, String target){
-          
+          HashMap<Character, Integer> map = new HashMap<>();
+          for(int i=0; i<source.length(); i++){
+              if(!map.containsKey(source.charAt(i)))
+                  map.put(source.charAt(i),1);
+              map.put(source.charAt(i), map.get(source.charAt(i))+1);
+          }
+          for(int i=0; i<target.length(); i++){
+              if(map.containsKey(target.charAt(i)) && map.get(target.charAt(i))>0)
+                  map.put(source.charAt(i), map.get(source.charAt(i))-1);
+              else
+                  return false;
+          }
+          for (Map.Entry<Character, Integer> set : map.entrySet()) {
+              if(set.getValue()==0)
+                  return false;
+          }
+          return true;
       }
 
     public static void main(String agrs[]){
         Scanner sc = new Scanner(System.in);
         String source = sc.nextLine();
         String target = sc.nextLine();
-        System.out.println(checkPermutation1(source,target));
+        System.out.println(checkPermutation2(source,target));
     }
 }
